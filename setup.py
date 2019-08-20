caa00d2c0939361915c8d74d5e7e170707e7780d
Karthik Rajasekaran <karthik@Karthiks-MacBook-Pro.local>
2019-08-19 17:59:21 -0700

    Singleton decorator + re-organization of tests directory structure

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
+from pyjamaparty.strutils.string_builder import StringBuilder
 
 description = 'Set of casual python utilities'
-long_description = '{}, written standing on shoulders of giants'.format(description)
+long_description = StringBuilder('{}, written standing on shoulders of giants.'.format(description))
+long_description += ' Tools include a string builder, singleton decorator'
 
 requirements = []
 setup(
    name='pyjamaparty',
-   version='0.1',
+   version='0.2',
    description=description,
    license="MIT",
-   long_description=long_description,
+   long_description=str(long_description),
    author='Karthik Rajasekaran',
    author_email='krajasek@gmail.com',
    url="http://github.com/krajasek/pyjamaparty",
